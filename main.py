e78d5d2216671270ed36456b5ceb67874362b2f8
Osbertt-19 <minhtetnaing25mhn@gmail.com>
2023-10-19 13:06:55 +0700

    deleted main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from shoze.core.cell import Cell
-from shoze.core.mazes.empty_maze import EmptyMaze
-from shoze.core.mazes.start_end_maze import StartEndMaze
-from shoze.core.mazes.start_maze import StartMaze
-from shoze.exporters.png_exporter import export_png
-
-
-maze = StartMaze((20, 20))
-export_png(maze)
